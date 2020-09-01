dbac21eb741c175ea615b93f5bff9c0f95e9244b
gzandiemer <g.z.diemer@gmail.com>
2020-09-01 19:13:47 +0200

    complete ex 1.9

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -3,19 +3,29 @@ import ReactDOM from 'react-dom'
 
 const Statistics = ({ good, neutral, bad }) => {
   const all = good + neutral + bad
-  const average = good * 1 + bad * -1
-  const positive = good / all * 100
+  if (all > 0) {
+    const average = good * 1 + bad * -1
+    const positive = good / all * 100
+    return (
+      <div>
+        <h2>statistics</h2>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+        <p>all {all}</p>
+        <p>average {average}</p>
+        <p>positive {positive || 0} </p>
+      </div>
+    )
+  }
+
   return (
     <div>
-      <h3>statistics</h3>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {average}</p>
-      <p>positive {positive || 0} </p>
+      <h2>statistics</h2>
+      <p>No feedback given</p>
     </div>
   )
+
 }
 
 const App = () => {
@@ -26,7 +36,7 @@ const App = () => {
 
   return (
     <div>
-      <h3>give feedback</h3>
+      <h2>give feedback</h2>
       <button onClick={() => setGood(good + 1)}>good</button>
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
