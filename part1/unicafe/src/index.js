5a4b12fdb285c6e568defbee4f1abb974af49c23
gzandiemer <g.z.diemer@gmail.com>
2020-09-01 19:02:15 +0200

    complete ex 1.7

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -7,7 +7,9 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
+  const all = good + neutral + bad
+  const average = good * 1 + bad * -1
+  const positive = good / all * 100
   return (
     <div>
       <h1>give feedback</h1>
@@ -18,6 +20,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive || 0} </p>
     </div >
   )
 }
